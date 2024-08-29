d963040e906a1cf4d8b083aa7b26783e7c2e99b2
Yash Dhiraj Oza <144452060+YashDhirajOza@users.noreply.github.com>
2024-08-29 22:34:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+VANTA.BIRDS({
+    el: "#vanta-bg",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    backgroundColor: 0x1e7ce8,
+    color1: 0xc400ff,
+    color2: 0x12dbdb,
+    colorMode: "variance",
+    birdSize: 1.70,
+    alignment: 13.00,
+    cohesion: 35.00
+  });
+  
